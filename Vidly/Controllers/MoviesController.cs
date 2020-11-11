1bc6e8a5a0cc16c0a4174eb8ee230d23e51655b2
vutsulug <nyikovutsulu@gmail.com>
2020-11-11 15:21:52 +0200

    First commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using 
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            return View();
+
+            
+        }
+    }
+}
